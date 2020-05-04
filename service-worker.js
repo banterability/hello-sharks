cd656c1ffd8dfa81c169d6eab17421cd705c093a
Jeff Long <banterability@users.noreply.github.com>
2020-05-04 11:39:11 -0500

    clean up old caches

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,28 @@
-const version = "v2";
+const version = "v3";
 const cacheName = `${version}-hellosharks`;
 
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/',
-        '/style.css',
-        '/script.js'
-      ]).then(() => self.skipWaiting());
+      return cache
+        .addAll(["/", "/style.css", "/script.js"])
+        .then(() => self.skipWaiting());
     })
   );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => !key.startsWith(version))
+            .map(key => caches.delete(key))
+        )
+      )
+  );
 });
 
 self.addEventListener("fetch", event => {
